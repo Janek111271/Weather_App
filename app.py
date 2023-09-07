1cbbb8087639d6e058bc5c7683b3f01ddd6d0b68
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2023-09-07 14:42:55 +0200

    code fixing, change of logic

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 app.secret_key = "12312311"
 Base = declarative_base()
 
+# Define your database URL
+DATABASE_URL = 'sqlite:///phone_book.db'
+
 
 class Book(Base):
     __tablename__ = 'books'
@@ -24,27 +27,43 @@ class PhoneForm(FlaskForm):
     city_code = StringField(label='City number', validators=[DataRequired()])
 
 
-engine = create_engine('sqlite:///phone_book.db')
+# Create the database engine and session
+engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
+def get_books_as_list():
+    books_list = []
+    books = session.query(Book).all()
+    for book in books:
+        book_dict = {
+            'id': book.id,
+            'phone_number': book.phone_n,
+            'city_code': book.city_c
+        }
+        books_list.append(book_dict)
+    return books_list
+
+
 @app.route('/')
 def start():
-    return redirect(url_for('home'))
+    return redirect(url_for('form'))
 
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
+@app.route('/form', methods=['GET', 'POST'])
+def form():
     phone_upload = PhoneForm()
+    number = get_books_as_list()
     if phone_upload.validate_on_submit():
-        phone = phone_upload['phone_number']
-        city = phone_upload['city_code']
+        phone = phone_upload.phone_number.data  # Access the form field using .data
+        city = phone_upload.city_code.data  # Access the form field using .data
         new_number = Book(phone_n=phone, city_c=city)
         session.add(new_number)
         session.commit()
-    return render_template('index.html', form=phone_upload)
+        return redirect(url_for('form'))
+    return render_template('form.html', form=phone_upload, list=number)
 
 
 if __name__ == '__main__':
